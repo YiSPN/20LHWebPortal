026a5c1ffc0625a6cea8d4d89a18a34660137bcf
qynvi <mllwnf@gmail.com>
2015-10-05 13:23:11 -0700

    Gravatar backend

---
+++ 20LHWebPortal/Models/UserGravatarModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace _20LHWebPortal.Models
+{
+	public class UserGravatarModel
+	{
+        public string GetGravatarUrl(object key) {
+            // string email = (string)DataBinder.Eval(key, "email");
+            string email = "mllwnf@gmail.com";
+            string hash = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(email.Trim(), "MD5");
+            hash = hash.Trim().ToLower();
+
+            return string.Format("http://www.gravatar.com/avatar.php?gravatar_id={0}&rating=G&size=60", hash);
+        }
+    }
+}
