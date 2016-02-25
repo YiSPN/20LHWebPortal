fb030c560ea8d4ddc8deb554aff04338898e13f4
Kishan Wanigasinghe <kwanigasinghe@quicksolutions.com>
2016-02-25 01:36:46 -0500

    event card styling+ partial view added, other ui styling, menu link added

--- 20LHWebPortal/Controllers/HomeController.cs
+++ 20LHWebPortal/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _20LHWebPortal.Models.Events;
 
 namespace _20LHWebPortal.Controllers
 {
@@ -33,6 +34,23 @@ namespace _20LHWebPortal.Controllers
         public ActionResult TestView()
         {
             return View();
+        }
+
+        public ActionResult Ui()
+        {
+            var model = new EventCard
+            {
+                EventId = 0,
+                Title = "Pickup Soccer",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                BannerImageUrl = "/Content/Images/event-banner.jpg",
+                Location = "Columbus, OH",
+                UserFullName = "John Doe",
+                UserImageUrl = "/Content/Images/user.png",
+                StartTime = DateTime.Now.AddDays(10),
+                EndTime = DateTime.Now.AddDays(10).AddHours(3)
+            };
+            return View(model);
         }
     }
 }
---
+++ 20LHWebPortal/Models/Events/EventCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _20LHWebPortal.Models.Events
+{
+    public class EventCard
+    {
+        public long EventId { get; set; }
+        public string Title { get; set; }
+        public string BannerImageUrl { get; set; }
+        public string UserImageUrl { get; set; }
+        public string UserFullName { get; set; } //First & Last name
+        public string Description { get; set; } //120 max length!
+        public string Location { get; set; } //City state for right now
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
