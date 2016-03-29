b71df003f9824ad3d0eaa2dc243563a4167529c7
Yi Mei <y.mei@tcac-usa.com>
2016-03-29 16:00:43 -0700

    Local time debug

--- 20LHWebPortal/Models/HangoutRepository.cs
+++ 20LHWebPortal/Models/HangoutRepository.cs
@@ -379,7 +379,7 @@ namespace _20LHWebPortal.Models
                     {
                         AspNetUserId = UserAccount.Id,
                         HangoutId = Hangout.Id,
-                        TimeStamp = DateTimeOffset.Now.DateTime,
+                        TimeStamp = DateTimeOffset.Now.DateTime.ToLocalTime(),
                         ActivityType = (int)ActivityType.Join
                     };
 
@@ -632,7 +632,7 @@ namespace _20LHWebPortal.Models
             {
                 AspNetUserId = model.UserId,
                 HangoutId = hang.Id,
-                TimeStamp = DateTimeOffset.Now.DateTime,
+                TimeStamp = DateTimeOffset.Now.DateTime.ToLocalTime(),
                 ActivityType = (int) ActivityType.Create
             };
 
